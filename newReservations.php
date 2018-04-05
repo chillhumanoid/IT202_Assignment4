1cecf2a47af82f623af7a01b40268988196342b2
Jonathan Thorne <jonathanthorne13@gmail.com>
2018-04-05 14:03:46 -0400

    new res form done, php handling needs work

--- newReservations.php
+++ newReservations.php
@@ -1,6 +1,7 @@
 <?php
 	session_start();
 	include 'include/header.php';
+  include 'include/config.php';
 	if(isset($_POST['options']) && $_POST['options']==='view'){
 		header('Location: https://web.njit.edu/~jgt8/Assignment4/newReservations.php');
     exit;
@@ -18,24 +19,185 @@
 			<form action="" method="POST">
 				<table>
 					<tr>
-						<th colspan="2">Event Center Portal</th>
+						<th colspan="2">ECP - New Reservation</th>
 					</tr>
 					<tr>
-						<td class = "labels">Customer ID:</td>
+						<td class = "labels">Event Type:</td>
 						<td>
-							<?php echo $_SESSION[id]; ?>
+							<select name = "eventType">
+                <option value="">
+                </option>
+                <option value="meeting">
+                  Meeting
+                </option>
+                <option value="conference">
+                  Conference
+                </option>
+                <option value="athletic">
+                  Athletic Event
+                </option>
+                <option value="meet">
+                  Meet and Greet
+                </option>
+                <option value="dinner">
+                  Dinner
+                </option>
+              </select>
 						</td>
 					</tr>
 					<tr>
-						<td>Select a transaction:</td>
+						<td>Date</td>
 						<td>
-						<select name = "options">
-							<option value="view">View Reservation</option>
-							<option value="new">New Reservations</option>
+              <input type ="date" name ="eventDate" />
 						</td>
 					</tr>
+          <tr>
+            <td class = "labels">
+              Supply Slot 1
+            </td>
+            <td>
+              <select name = "supplies1">
+                <option value="">
+                </option>
+                <option value="chairs">
+                  Chairs
+                </option>
+                <option value="tables">
+                  Tables
+                </option>
+                <option value="food">
+                  Food
+                </option>
+                <option value="drink">
+                  Drink
+                </option>
+                <option value="entertainment">
+                  Entertainment
+                </option>
+                <option value="decorations">
+                  Decorations
+                </option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td class = "labels">
+              Supply Slot 2
+            </td>
+            <td>
+              <select name = "supplies2">
+                <option value="">
+                </option>
+                <option value="chairs">
+                  Chairs
+                </option>
+                <option value="tables">
+                  Tables
+                </option>
+                <option value="food">
+                  Food
+                </option>
+                <option value="drink">
+                  Drink
+                </option>
+                <option value="entertainment">
+                  Entertainment
+                </option>
+                <option value="decorations">
+                  Decorations
+                </option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td class = "labels">
+              Supply Slot 3
+            </td>
+            <td>
+              <select name = "supplies3">
+                <option value="">
+                </option>
+                <option value="chairs">
+                  Chairs
+                </option>
+                <option value="tables">
+                  Tables
+                </option>
+                <option value="food">
+                  Food
+                </option>
+                <option value="drink">
+                  Drink
+                </option>
+                <option value="entertainment">
+                  Entertainment
+                </option>
+                <option value="decorations">
+                  Decorations
+                </option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td class = "labels">
+              Supply Slot 4
+            </td>
+            <td>
+              <select name = "supplies4">
+                <option value="">
+                </option>
+                <option value="chairs">
+                  Chairs
+                </option>
+                <option value="tables">
+                  Tables
+                </option>
+                <option value="food">
+                  Food
+                </option>
+                <option value="drink">
+                  Drink
+                </option>
+                <option value="entertainment">
+                  Entertainment
+                </option>
+                <option value="decorations">
+                  Decorations
+                </option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td class = "labels">
+              Supply Slot 5
+            </td>
+            <td>
+              <select name = "supplies5">
+                <option value="">
+                </option>
+                <option value="chairs">
+                  Chairs
+                </option>
+                <option value="tables">
+                  Tables
+                </option>
+                <option value="food">
+                  Food
+                </option>
+                <option value="drink">
+                  Drink
+                </option>
+                <option value="entertainment">
+                  Entertainment
+                </option>
+                <option value="decorations">
+                  Decorations
+                </option>
+              </select>
+            </td>
+          </tr>
 					<tr>
-						<td colspan="3"><input class = "buttons" type="submit" value="Continue"></td>
+						<td colspan="3"><input class = "buttons" type="submit" value="Submit"></td>
 					</tr>
 				</table>
 			</form>
